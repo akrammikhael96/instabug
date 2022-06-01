a0ecf8fa5ab6837c0fb85a268b5bdb2e31ed2def
“[Akram <[akrammikhael96@gmail.com]>
2022-06-01 13:54:00 +0200

    updatedPath

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/instabug-login-page/",
+  base: "/instabug/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
